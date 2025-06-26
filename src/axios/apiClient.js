c4572d282ae8e7cc50c1df71ca93028803ae04dc
Beknur <b4476125@gmail.com>
2025-06-27 00:32:53 +0600

    hh

---
+++ src/axios/apiClient.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+    baseURL: "https://6824116e65ba05803398c680.mockapi.io/products"
+})
+
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import { apiClient } from '../../axios/apiClient'
 
 function Header() {
+
+async function getCategory() {
+  try {
+    const res = await apiClient.get("https://6824116e65ba05803398c680.mockapi.io/products/category-list")
+
+    console.log(res);
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+useEffect(() => {
+  getCategory()
+} ,[])
   return (
     <div>
       <Link to="/">Home</Link>
